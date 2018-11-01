a0a9b9b6ac55b938164aae2999ab9b9d6b980335
TLXngo <tlxngo@163.com>
2018-11-01 14:32:48 +0800

    'one'

---
+++ df_user/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class UserInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=40)
+    uemail = models.CharField(max_length=40)
